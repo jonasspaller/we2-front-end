8d08bd21904c80fb73e265cdc39c8c938653700a
Jonas Spaller <hey@jonasspaller.de>
2022-05-25 08:49:29 +0200

    modal doesnt work

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -2,8 +2,12 @@ import { Modal, Button } from 'react-bootstrap'
 
 function LoginModal(props) {
 
+	useEffect(() => {
+		props.setShowLoginModal(false)
+	}, [])
+
 	return (
-		<Modal.Dialog centered>
+		<Modal show={props.showLoginModal} centered>
 			<Modal.Header closeButton>
 				<Modal.Title>Modal title</Modal.Title>
 			</Modal.Header>
@@ -13,10 +17,10 @@ function LoginModal(props) {
 			</Modal.Body>
 
 			<Modal.Footer>
-				<Button variant="secondary" onClick={handleClose}>Close</Button>
-				<Button variant="primary" onClick={handleClose}>Save changes</Button>
+				<Button variant="secondary" onClick={this.handleClick}>Close</Button>
+				<Button variant="primary" onClick={this.handleClick}>Save changes</Button>
 			</Modal.Footer>
-		</Modal.Dialog>
+		</Modal>
 	)
 }
 
